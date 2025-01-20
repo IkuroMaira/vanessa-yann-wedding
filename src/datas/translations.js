716b6295f9dbac2f27afa1a25bb1f416738567af
Gwenaëlle Bussac <gwen.bussac@gmail.com>
2025-01-20 18:50:20 +0100

    add switch language

---
+++ src/datas/translations.js
@@ -0,0 +1,49 @@
+export const translations = {
+    fr: [
+        {
+            buttonText: "Passer en français",
+            title: {
+                save: "Save",
+                the: "the",
+                date: "date"
+            },
+            weddingInfo: {
+                line1: "Pour la mariage",
+                line2: "Vanessa & Yann",
+                weddingDate: "Le 26 juillet 2025,",
+                location: "à Gréoux-Les-Bains"
+            },
+            invitationText: {
+                paragraph1: "Vous êtes invités à notre mariage qui se déroulera le 26 juillet 2025 au Pavillon d'Aurabelle à Gréoux-Les-Bains.",
+                paragraph2: "Les informations complémentaires suivront bientôt."
+            },
+            formReminder: {
+                text: "Pensez à remplir le formulaire."
+            }
+        },
+    ],
+    en: [
+        {
+            buttonText: "Switch to english",
+            title: {
+                save: "Save",
+                the: "the",
+                date: "date"
+            },
+            weddingInfo: {
+                line1: "test en anglais",
+                line2: "",
+                date: "",
+                location: ""
+            },
+            invitationText: {
+                paragraph1: "",
+                paragraph2: ""
+            },
+            formReminder: {
+                text: ""
+            }
+        },
+    ]
+};
+
--- src/scripts/setupLanguage.js
+++ src/scripts/setupLanguage.js
@@ -1,21 +1,42 @@
-import { languages } from '../datas/language.js';
+import { translations } from '../datas/translations.js';
 
-// Accés à la version en français
-const translateFR = languages.fr;
-console.log(translateFR);
+let currentLang = 'fr';
 
-// Accés à la version en anglais
-const translateEN = languages.en;
-// console.log(translateEN);
-
-console.log(translateFR[0].title.save);
-
-export function initLanguageSwitch(languages) {
+export function initLanguageSwitch() {
     const langLink = document.querySelector('.lang-switch');
+    // console.log(langLink);
     if (!langLink) return;
+
+    updateButtonText(langLink);
 
     langLink.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log('Changement de langue');
+
+        currentLang = currentLang === 'fr' ? 'en' : 'fr';
+        // console.log('La langue par défaut', currentLang);
+
+        updateButtonText(langLink);
+        updateTextPage(currentLang);
     });
+}
+
+function updateButtonText(button) {
+    const targetLang = currentLang === 'fr' ? `${translations.en[0].buttonText}` : `${translations.fr[0].buttonText}`;
+    button.textContent = `${targetLang}`;
+    // console.log(targetLang);
+}
+
+function updateTextPage(language) {
+    if (language === 'fr') {
+        // console.log("On change la langue en français !", language);
+        // console.log(translations.fr[0].buttonText);
+
+        document.getElementById('save').textContent = translations.fr[0].title.save;
+        document.getElementById('the').textContent = translations.fr[0].title.the;
+        document.getElementById('date').textContent = translations.fr[0].title.date;
+        document.getElementById('line1').textContent = translations.fr[0].weddingInfo.line1;
+        document.getElementById('line2').textContent = translations.fr[0].weddingInfo.line2;
+    } else if (language === 'en') {
+
+    }
 }
