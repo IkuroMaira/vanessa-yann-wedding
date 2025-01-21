67cfd7e029af81033de0171cda534f305595fc2f
ymougenel <ymougenel@takima.fr>
2025-01-21 21:33:48 +0100

    feat: complete English translation

--- src/datas/translations.js
+++ src/datas/translations.js
@@ -11,10 +11,10 @@ export const translations = {
                 line1: "Pour le mariage",
                 line2: "Vanessa & Yann",
                 weddingDate: "Le 26 juillet 2025,",
-                location: "à Gréoux-Les-Bains"
+                location: "à Bouc-Bel-Air"
             },
             invitationText: {
-                paragraph1: "Vous êtes invités à notre mariage qui se déroulera le 26 juillet 2025 au Pavillon d'Aurabelle à Gréoux-Les-Bains.",
+                paragraph1: "Vous êtes invités à notre mariage qui se déroulera le 26 juillet 2025 à Bouc-Bel-Air pour la mairie, puis au Pavillon d'Aurabelle à Gréoux-Les-Bains.",
                 paragraph2: "Les informations complémentaires suivront bientôt."
             },
             formReminder: {
@@ -24,24 +24,24 @@ export const translations = {
     ],
     en: [
         {
-            buttonText: "Switch to english",
+            buttonText: "Switch to English",
             title: {
                 save: "Save",
                 the: "the",
                 date: "date"
             },
             weddingInfo: {
-                line1: "test en anglais",
-                line2: "Vannessa & Yann",
-                weddingDate: "",
-                location: ""
+                line1: "For the wedding of",
+                line2: "Vanessa & Yann",
+                weddingDate: "The 26th of July",
+                location: "at Bouc-Bel-Air"
             },
             invitationText: {
-                paragraph1: "",
-                paragraph2: ""
+                paragraph1: "We have the pleasure to invite you at our wedding, we will first gather at the Bouc-Bel-Air town hall. Following this, we will head to the reception venue at Pavillon d'Aurabelle in Gréoux-Les-Bains.",
+                paragraph2: "Additional information will follow soon."
             },
             formReminder: {
-                text: "",
+                text: "Please remember to fill out the form with your personal information.",
             }
         },
     ]
