4c66738923a99fb600e64ec1c584299a2088555b
IkuroMaira <gwen.bussac@gmail.com>
2025-04-29 15:06:05 +0200

    Remove CSS tonot having horizontal scroll

--- front-react/src/main.jsx
+++ front-react/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.module.css'
 import './styles/variables.module.css';
-import './styles/global-fixes.module.css';
 import App from './App.jsx'
 import LanguageContextProvider from "./contexts/LanguageContext.jsx";
 
