fac522293810541d17721803a75611293f9bc79b
IkuroMaira <gwen.bussac@gmail.com>
2025-04-29 15:03:05 +0200

    Another test for not having scroll

--- front-react/src/main.jsx
+++ front-react/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.module.css'
-import './styles/variables.css';
+import './styles/variables.module.css';
+import './styles/global-fixes.module.css';
 import App from './App.jsx'
 import LanguageContextProvider from "./contexts/LanguageContext.jsx";
 
